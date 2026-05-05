22305a4b4c283fc4458fee7333e421aec2e2a869
JaidaT <jtarlit3@my.bcit.ca>
2026-05-05 11:56:06 -0700

    version: includes Payment Made

--- script.js
+++ script.js
@@ -1,2 +1,67 @@
 // alert("Bonour");
 
+// --- SUMMARY --- //
+// const summaryU1Total = document.getElementById("summaryU1Total");
+const summaryU1Owed = document.getElementById("summaryU1Owed");
+// const summaryU2Total = document.getElementById("summaryU2Total");
+const summaryU2Owed = document.getElementById("summaryU2Owed");
+
+// --- ADD EXPENSE --- //
+// user select input
+const expenseNameInput = document.getElementById("expenseNameInput");
+const expenseAmountInput = document.getElementById("expenseAmountInput");
+const costShareInput = document.getElementById("costShareInput");
+const addExpenseBtn = document.getElementById("addExpenseBtn");
+
+// --- CURRENCY CONVERTER --- //
+
+
+// --- PAYMENT MADE --- //
+// from user select
+// to user select
+const paymentAmountInput = getElementById("paymentAmountInput");
+const logPaymentBtn = getElementById("logPaymentBtn");
+
+// --- HISTORY --- //
+const historyList = document.getElementById("historyList");
+
+
+// --- FUNCTIONS --- //
+function displaySummary() {
+    // 
+}
+
+
+function addExpense() {
+    // take user select input
+    // take expense name and save as string
+    // take total expense amount and save as number
+    // take cost split %, change to decimal, and save as number
+    // 
+}
+
+function currencyConvert() {
+    // take base currency 
+    // take currency to convert to 
+    // pass base currency to API
+    // request converted currency response from API 
+}
+
+function paymentMade() {
+    // take user "from" select input
+    // take user "to" select input
+    // take 
+}
+
+function displayExpenseLog() {
+    // forEach expense, display date, expense name, 
+    // total expense amt, user1's amount paid, and user2's amount paid
+}
+
+function calcSplit() {
+    // 
+}
+
+function calcSummary() {
+    // 
+}
