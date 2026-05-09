fd7e98446d501901ecc3cf6d5bcf41741b43caed
JaidaT <jtarlit3@my.bcit.ca>
2026-05-09 12:54:03 -0700

    Add functions to update usernames

--- script.js
+++ script.js
@@ -1,37 +1,89 @@
 // alert("Bonour");
 
-// --- SUMMARY --- //
-// const summaryU1Total = document.getElementById("summaryU1Total");
-const summaryU1Owed = document.getElementById("summaryU1Owed");
-// const summaryU2Total = document.getElementById("summaryU2Total");
-const summaryU2Owed = document.getElementById("summaryU2Owed");
+// --- DOM ELEMENTS --- //
+// SUMMARY //
+const u1CurrentlyOwed = document.getElementById("u1CurrentlyOwed");
+const u2CurrentlyOwed = document.getElementById("u2CurrentlyOwed");
+const clearOwedBtn = document.getElementById("clearOwedBtn");
 
-// --- ADD EXPENSE --- //
-// user select input
+// ADD EXPENSE //
+const addExpenseUserSelect = document.getElementById("addExpenseUserSelect");
 const expenseNameInput = document.getElementById("expenseNameInput");
 const expenseAmountInput = document.getElementById("expenseAmountInput");
 const costShareInput = document.getElementById("costShareInput");
 const addExpenseBtn = document.getElementById("addExpenseBtn");
 
-// --- EDIT USERS --- ///
+// EDIT USERS ///
 const editUser1Input = document.getElementById("editUser1Input");
 const editUser2Input = document.getElementById("editUser2Input");
-const saveUsersBtn = document.getElementById("saveUsersBtn")
+const saveUsersBtn = document.getElementById("saveUsersBtn");
 
-// --- CURRENCY CONVERTER --- //
+// CURRENCY CONVERTER //
+const convertBtn = document.getElementById("convertBtn")
 
-
-// --- HISTORY --- //
+// HISTORY //
 const historyList = document.getElementById("historyList");
+const convertFromSelect = document.getElementById("convertFromSelect");
+const convertToSelect = document.getElementById("convertToSelect");
 
+// --- VARIABLES --- //
+let user1name = JSON.parse(localStorage.getItem("user1name")) || "User 1";
+let user2name = JSON.parse(localStorage.getItem("user2name")) || "User 2";
 
+const expenseHistory = [
+    { date: "", expenseName = "", totalAmt = 0, user1 = user1name, user2 = user2name}
+];
+
+// --- ON PAGE LOAD --- //
+updateUsernames();
 
 
 // --- FUNCTIONS --- //
 function editUser() {
     // takes inputs and replaces them as the value for each user
     // if a field is empty, dont replace existing value
+    const newName1 = editUser1Input.value.trim();
+    const newName2 = editUser2Input.value.trim();
+
+
+    if (newName1 !== "") {
+        user1name = newName1;
+        localStorage.setItem("user1name", JSON.stringify(user1name));
+    }
+
+    if (newName2 !== "") {
+        user2name = newName2;
+        localStorage.setItem("user2name", JSON.stringify(user2name));
+    }
+    
+    console.log(user1name, user2name)
+
     // refresh all dislays to show updated name
+    updateUsernames();
+}
+
+function updateUsernames() {
+    // update Add Expense User <option>s
+    const newLabelU1 = user1name;
+    const newValueU1 = user1name;
+
+    const newLabelU2 = user2name;
+    const newValueU2 = user2name;
+
+    let addExpenseUserOption1 = document.getElementById("addExpenseUserSelect").options[0];
+    addExpenseUserOption1.text = user1name;
+    addExpenseUserOption1.value = user1name;
+
+    let addExpenseUserOption2 = document.getElementById("addExpenseUserSelect").options[1];
+    addExpenseUserOption2.text = newLabelU2;
+    addExpenseUserOption2.value = newValueU2;
+
+    // update Currently Owed user labels
+    let owedUser1Label = document.getElementById("owedUser1Label");
+    let owedUser2Label = document.getElementById("owedUser2Label");
+
+    owedUser1Label.textContent = user1name;
+    owedUser2Label.textContent = user2name;
 }
 
 function addExpense() {
@@ -57,3 +109,5 @@ function displayHistory() {
 function displaySummary() {
     // 
 }
+
+saveUsersBtn.addEventListener("click", editUser)
